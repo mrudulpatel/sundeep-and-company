f81f0c338f65bfa31655e4cd2baba90359adc8a9
Mrudul Patel <mrudulpatel04@gmail.com>
2025-03-29 13:08:36 +0530

    Speed Insights & Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import { Space_Grotesk } from "next/font/google";
-import "./globals.css";
 import Footer from "@/components/Footer";
+import "./globals.css";
 
 const space_grotesk = Space_Grotesk({
   variable: "--font-space-grotesk",
@@ -27,6 +29,8 @@ export default function RootLayout({
       >
         {/* <Navbar /> */}
         <main>{children}</main>
+        <SpeedInsights />
+        <Analytics />
         <Footer />
       </body>
     </html>
